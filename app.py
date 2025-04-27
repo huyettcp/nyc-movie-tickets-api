d047ac04aa283a1ca3eb5ba2d3d6ffd2c99760c8
huyettcp <huyettcp@gmail.com>
2025-04-27 15:32:58 -0400

    Pin uc version and add --disable-gpu for stability

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def scrape_nyc_movie_showtimes():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")  
 
     driver = uc.Chrome(options=options, use_subprocess=True)
     all_showings = []
